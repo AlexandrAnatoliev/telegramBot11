88a651d2b82c18d425249b5904904ab36ca902a9
AlexandrAnatoliev <per-1986@list.ru>
2023-02-22 20:04:07 +0300

    Модифицируйте бота, добавив функцию обработки команды /description, которая будет выводить описание бота

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 # telegramBot11
-# Обработка команд /start и /help, /description, которая будет выводить описание бота.
+# Обработка команд /start, /help и /description, которая будет выводить описание бота.
 # Бот отвечает на сообщение пользователя рандомным символом алфавита.
 # https://www.youtube.com/watch?v=VWjCv_IDuyQ&list=PLe-iIMbo5JOJm6DRTjhleHojroS-Bbocr&index=4
-import random
 
 # ЗАМЕЧАНИЯ:
 # message.reply - ответить на сообщение
@@ -13,12 +12,17 @@ import random
 # Напишите бота, который будет отвечать на сообщение пользователя рандомным символом алфавита.
 # Модифицируйте бота, добавив функцию обработки команды /description, которая будет выводить описание бота
 
+import random
 from aiogram import Bot, Dispatcher, types, executor
 from config import TOKEN_API
 
 HELP_COMMAND = """
 /help - список команд
 /start - начать работу с ботом
+"""
+DESCRIPTION_COMMAND = """
+Бот отвечает на сообщение пользователя рандомным символом алфавита.
+Обрабатывает команды /start, /help и /description, которая будет выводить описание бота.
 """
 
 bot = Bot(TOKEN_API)
@@ -34,6 +38,11 @@ async def command(message: types.Message):
 @dp.message_handler(commands='help')
 async def command(message: types.Message):
     await message.reply(text=HELP_COMMAND)
+
+
+@dp.message_handler(commands='description')
+async def command(message: types.Message):
+    await message.reply(text=DESCRIPTION_COMMAND)
 
 
 @dp.message_handler()  # обрабатываем входящие сообщения
